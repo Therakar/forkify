cac9168d9e24ef0be652dc6b75514bfaf1af4991
Therakar <matteo.pau200@gmail.com>
2023-06-07 14:20:44 +0200

    refactoring for MVC

---
+++ src/js/model.js
@@ -0,0 +1,37 @@
+/*
+This is gonna be the module in 
+wich I'll write my entire model
+*/
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+    console.log(res, data);
+
+    //re-format data object
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+
+    //creation of the custom error
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+  } catch (err) {
+    alert(err);
+  }
+};
