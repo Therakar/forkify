6516487c0be29fb8dead74469cb63c0a923b6a2f
Therakar <matteo.pau200@gmail.com>
2023-06-07 14:34:43 +0200

    reformat ingredient's numeric values

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -5,6 +5,7 @@ some private methods and properties.
 */
 
 import icons from 'url:../../img/icons.svg';
+import { Fraction } from 'fractional';
 
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
@@ -95,20 +96,7 @@ class RecipeView {
           <ul class="recipe__ingredient-list">
 
             ${this.#data.ingredients
-              .map(ing => {
-                return `
-                <li class="recipe__ingredient">
-                  <svg class="recipe__icon">
-                    <use href="${icons}#icon-check"></use>
-                  </svg>
-                  <div class="recipe__quantity">${ing.quantity}</div>
-                  <div class="recipe__description">
-                    <span class="recipe__unit">${ing.unit}</span>
-                    ${ing.description}
-                  </div>
-                </li>
-              `;
-              })
+              .map(this.#generateMarkupIngredient)
               .join('')}
 
             <li class="recipe__ingredient">
@@ -144,6 +132,22 @@ class RecipeView {
             </svg>
           </a>
         </div>
+    `;
+  }
+  #generateMarkupIngredient(ing) {
+    return `
+      <li class="recipe__ingredient">
+        <svg class="recipe__icon">
+          <use href="${icons}#icon-check"></use>
+        </svg>
+        <div class="recipe__quantity">${
+          ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        }</div>
+        <div class="recipe__description">
+          <span class="recipe__unit">${ing.unit}</span>
+          ${ing.description}
+        </div>
+      </li>
     `;
   }
 }
